17bcf3a858854d14b4027fb7ee9f58567bf12a6c
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-03-28 20:27:22 +0300

    BasicSyntax

---
+++ BasicSyntax/Division/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int divisible = -1;
+            if(num % 10 == 0)
+            {
+                divisible = 10;
+            }
+            else if(num % 7 ==0)
+            {
+                divisible = 7;
+            }
+            else if(num % 6 == 0)
+            {
+                divisible = 6;
+            }
+            else if(num % 3 == 0)
+            {
+                divisible = 3;
+            }
+            else if(num % 2 == 0)
+            {
+                divisible = 2;
+            }
+            if(divisible != -1)
+            {
+                Console.WriteLine($"The number is divisible by {divisible}");
+            }
+            else
+            {
+                Console.WriteLine("Not divisible");
+            }
+        }
+    }
+}
