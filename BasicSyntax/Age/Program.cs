4e4a02164b82133a62e433c312d29ae923e422c0
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-03-28 20:24:54 +0300

    BasicSyntax

---
+++ BasicSyntax/Age/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int years = int.Parse(Console.ReadLine());
+            string age = string.Empty;
+            if(years >= 0 && years <= 2)
+            {
+                age = "baby";
+            }
+            else if(years >= 3 && years <= 13)
+            {
+                age = "child";
+            }
+            else if(years >= 14 && years <= 19)
+            {
+                age = "teenager";
+            }
+            else if(years >= 20 && years <= 65)
+            {
+                age = "adult";
+            }
+            else
+            {
+                age = "elder";
+            }
+
+            Console.WriteLine(age);
+
+        }
+    }
+}
