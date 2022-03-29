c67533d7ab45bbe0c0c8fdb69744ca75f1064216
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-03-29 19:19:51 +0300

    BasicSyntax

---
+++ BasicSyntax/PrintAndSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrintAndSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                if (i == end)
+                {
+                    Console.WriteLine(i + " ");
+                    sum += i; 
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                    sum += i;
+                }
+
+
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
