3dc00e995a50151c4e1d2863ee7429461b5dfd39
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-03-29 19:19:08 +0300

    BasicSyntax

---
+++ BasicSyntax/Login/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            int counter = 0;
+            string password = string.Empty;
+            string passwordInput = string.Empty;
+            for (int reverseIndex = username.Length - 1; reverseIndex >= 0; reverseIndex--)
+            {
+                password += (username[reverseIndex]);
+            }
+            for (int count = 1; count <= 4; count++)
+            {
+                passwordInput = Console.ReadLine();
+
+                if (passwordInput == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else
+                {
+                    if (count == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Incorrect password. Try again.");
+                        continue; 
+                    }
+                }
+            }
+        }
+    }
+}
