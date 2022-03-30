5b1b412d21318a629803b97113386c2ab29b3b2b
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-03-30 19:39:18 +0300

    Arrays

---
+++ ArraysExercise/ArrayRotations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayRotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rotationsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rotationsCount; i++)
+            {
+                int firstEl = array[0];
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+                array[array.Length - 1] = firstEl;
+            }
+            Console.WriteLine(string.Join(' ', array));
+        }
+    }
+}
