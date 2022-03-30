098c83a0c75aa659cd846e71de436d79eaec7165
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-03-30 19:40:34 +0300

    Arrays

---
+++ ArraysExercise/EqualSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int leftSum = 0;
+            int rightSum = 0;
+            bool isTrue = false;
+
+            if(input.Length == 1)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                leftSum = input.Take(i).Sum();
+                rightSum = input.Skip(i + 1).Sum();
+
+                if(leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    isTrue = true;
+                    break;
+                }
+            }
+            if(!isTrue)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ ArraysExercise/MagicSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
