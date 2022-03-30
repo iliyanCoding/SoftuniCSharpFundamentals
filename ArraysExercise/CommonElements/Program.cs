37e5865673c23fbf68def959956e86b143280bbb
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-03-30 19:40:02 +0300

    Arrays

---
+++ ArraysExercise/CommonElements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] array1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] array2 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+              
+            var intersect = array2.Intersect(array1);
+
+            foreach (string value in intersect)
+            {
+                Console.Write(value + " ");
+            }
+
+        }
+    }
+}
