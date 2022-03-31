f7d042e9c10e0140c78f841f7e9b09886ee12e72
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-03-31 23:38:36 +0300

    Arrays

---
+++ ArraysExercise/train/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int[] numOfPassengers = new int[wagons];
+            int totalPassengers = 0;
+            for (int i = 0; i < numOfPassengers.Length; i++)
+            {
+                numOfPassengers[i] = int.Parse(Console.ReadLine());
+                totalPassengers += numOfPassengers[i];
+            }
+            Console.WriteLine(String.Join(' ', numOfPassengers));
+            Console.WriteLine(totalPassengers);
+        }
+    }
+}
