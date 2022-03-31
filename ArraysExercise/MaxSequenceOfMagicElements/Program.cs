cf378c69a857ee8de21529abaa46fdab9833c982
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-03-31 23:38:07 +0300

    Arrays

---
+++ ArraysExercise/MaxSequenceOfMagicElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace MaxSequenceOfMagicElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int counter = 0;
+            int winningCounter = 0;
+            string number = string.Empty;
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if(input[i] == input[i + 1])
+                {
+                    counter++;
+                    if (counter > winningCounter)
+                    {
+                        winningCounter = counter;
+                        number = input[i].ToString();
+                    }
+                }
+
+               
+                else
+                {
+                    counter = 0;
+                }
+                
+            }
+            for (int i = 0; i <= winningCounter; i++)
+            {
+                Console.Write(number + " ");
+            }
+        }   
+    }
+}
---
+++ ArraysExercise/TopIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+
+            for (int i = 0; i <array.Length; i++)
+            {
+                bool isItTopInteger = true;
+
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i] <= array[j])
+                    { 
+                        isItTopInteger = false;
+                    }
+                }
+                
+                if(isItTopInteger)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+
+        }
+    }
+}
