6939c0a829ca961940bab15f4d4e70b24a24c8dd
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-04-01 16:20:42 +0300

    AssociativeArrays

---
+++ AssociativeArraysExercise/04.Orders/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.Orders
+{
+    class Product
+    {
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, Product> productsDictionary = new Dictionary<string, Product>();
+            while (input != "buy")
+            {
+                string[] productArgs = input.Split(' ');
+                string productName = productArgs[0];
+                Product product = new Product()
+                {
+                    Price = double.Parse(productArgs[1]),
+                    Quantity = int.Parse(productArgs[2])
+                    
+                };
+                if (!productsDictionary.ContainsKey(productName))
+                {
+                    productsDictionary.Add(productName, product);
+                }
+                else
+                {
+                    productsDictionary[productName].Quantity += product.Quantity;
+                    if (productsDictionary[productName].Price != product.Price)
+                    {
+                        productsDictionary[productName].Price = product.Price;
+                    }
+                }
+
+                input = Console.ReadLine();
+
+            }
+
+            foreach (var item in productsDictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value.Price * item.Value.Quantity:f2}");
+            }
+        }
+    }
+}
---
+++ AssociativeArraysExercise/05.SoftuniParking/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05.SoftuniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, string> parkingInfo = new Dictionary<string, string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string command = input[0];
+                if(command == "register")
+                {
+                    string username = input[1];
+                    string licensePlateNumber = input[2];
+                    if(parkingInfo.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                    else
+                    {
+                        parkingInfo.Add(username, licensePlateNumber);
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                }
+                else
+                {
+                    string username = input[1];
+                    if (parkingInfo.ContainsKey(username))
+                    {
+                        parkingInfo.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                }
+            }
+
+            foreach (var item in parkingInfo)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+    }
+}
