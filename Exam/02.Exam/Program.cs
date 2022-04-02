662371adb84d6e959270ff6bbdb6018f4b781e9f
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-04-02 11:47:24 +0300

    Exam

---
+++ Exam/02.Exam/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02.Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double neededXP = double.Parse(Console.ReadLine());
+            int countOfBatles = int.Parse(Console.ReadLine());
+            double gatheredXP = 0;
+            for (int i = 1; i <= countOfBatles; i++)
+            {
+                double earnedXPInBatle = double.Parse(Console.ReadLine());
+                if(i % 3 == 0)
+                {
+                    gatheredXP += earnedXPInBatle + earnedXPInBatle * 0.15;
+                }
+                else if(i % 5 == 0)
+                {
+                    gatheredXP += earnedXPInBatle - earnedXPInBatle * 0.10;
+                }
+                else if(i % 15 == 0)
+                {
+                    gatheredXP += earnedXPInBatle + earnedXPInBatle * 0.05;
+                }
+                else
+                {
+                    gatheredXP += earnedXPInBatle;
+                }
+
+                if(gatheredXP >= neededXP)
+                {
+                    Console.WriteLine($"Player successfully collected his needed experience for {i} battles.");
+                    break;
+                }
+
+            }
+            if(gatheredXP < neededXP)
+            {
+                Console.WriteLine($"Player was not able to collect the needed experience, {(neededXP - gatheredXP):f2} more needed.");
+            }
+        }
+    }
+}
---
+++ Exam/03.Exam/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _03.Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] travelRoute = Console.ReadLine().Split("||");
+            int amountOfFuel = int.Parse(Console.ReadLine());
+            int amountOfAmmo = int.Parse(Console.ReadLine());
+            int amountOfAmmoOrFuel = 0;
+
+            for (int i = 0; i < travelRoute.Length; i++)
+            {
+                string[] currRoute = travelRoute[i].Split(' ');
+                string currCommand = currRoute[0];
+                if (currCommand != "Titan")
+                {
+                    amountOfAmmoOrFuel = int.Parse(currRoute[1]);
+                }
+
+
+                switch (currCommand)
+                {
+                    case "Travel":
+                        if (amountOfFuel >= amountOfAmmoOrFuel)
+                        {
+                            Console.WriteLine($"The spaceship travelled {amountOfAmmoOrFuel} light-years.");
+                            amountOfFuel -= amountOfAmmoOrFuel;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mission failed.");
+                            return;
+                        }
+                        break;
+                    case "Enemy":
+                        if (amountOfAmmo >= amountOfAmmoOrFuel)
+                        {
+                            Console.WriteLine($"An enemy with {amountOfAmmoOrFuel} armour is defeated.");
+                            amountOfAmmo -= amountOfAmmoOrFuel;
+                        }
+                        else
+                        {
+                            if (amountOfAmmoOrFuel * 2 <= amountOfFuel)
+                            {
+                                Console.WriteLine($"An enemy with {amountOfAmmoOrFuel} armour is outmaneuvered.");
+                                amountOfFuel -= amountOfAmmoOrFuel * 2;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Mission failed.");
+                                return;
+                            }
+                        }
+                        break;
+                    case "Repair":
+                        amountOfFuel += amountOfAmmoOrFuel;
+                        amountOfAmmo += amountOfAmmoOrFuel * 2;
+                        Console.WriteLine($"Ammunitions added: {amountOfAmmoOrFuel * 2}.");
+                        Console.WriteLine($"Fuel added: {amountOfAmmoOrFuel}.");
+                        break;
+                    case "Titan":
+                        Console.WriteLine("You have reached Titan, all passengers are safe.");
+                        break;
+                }
+            }
+
+        }
+    }
+}
