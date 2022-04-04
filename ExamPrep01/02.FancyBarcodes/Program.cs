b955794fcb455b77ac2841b43f8c1873894bc53a
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-04-04 11:27:14 +0300

    ExamPrep01

---
+++ ExamPrep01/02.FancyBarcodes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.FancyBarcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pathern = new Regex(@"@#+[A-Z][A-Za-z\d]{4,}[A-Z]@#+");
+            int numOfBarcodes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfBarcodes; i++)
+            {
+                string barcode = Console.ReadLine();
+
+                if(pathern.IsMatch(barcode))
+                {
+                    char[] digits = barcode.Where(x => char.IsDigit(x)).ToArray();
+
+                    if(digits.Length == 0)
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {string.Join("", digits)}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+            
+        }
+    }
+}
