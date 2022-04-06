0710aea7ad9a2cec6fac23b9848db5a0a458da9d
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-04-06 12:49:52 +0300

    ExamPrep02

---
+++ ExamPrep02/02.MirroWords/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.MirroWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pathern = new Regex(@"(@|#){1,2}\w{3,}\1(@|#){1,2}\w{3,}\1");
+            string input = Console.ReadLine();
+            
+        }
+    }
+}
