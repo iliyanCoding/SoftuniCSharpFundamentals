977301cbd7b7996257943e5b25ac93666327461d
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-04-08 12:41:30 +0300

    ExamPreparation

---
+++ ExamPreparation/MemoryGame/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MemoryGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> allElements = Console.ReadLine().Split(' ').ToList();
+            int movesCounter = 0;
+            bool outOfRange = false;
+            bool gameIsWon = false;
+
+            string input = Console.ReadLine();
+            while(input != "end")
+            {
+                int[] currElements = input.Split(' ').Select(int.Parse).ToArray();
+                //if(currElements[1] > currElements[0])
+                //{
+                //    currElements.Reverse();
+                //}
+                movesCounter++;
+                if(currElements[0] == currElements[1] || currElements[0] > allElements.Count || currElements[1] > allElements.Count || currElements[0] < 0 || currElements[1] < 0)
+                {
+                    string cheatElements = - + movesCounter + "a";
+                    allElements.Insert(allElements.Count / 2, cheatElements);
+                    allElements.Insert(allElements.Count / 2, cheatElements);
+                    Console.WriteLine("Invalid input! Adding additional elements to the board");
+                    outOfRange = true;
+                    
+                }
+                if(outOfRange == false)
+                {
+                    if (allElements[currElements[0]] == allElements[currElements[1]])
+                    {
+                        Console.WriteLine($"Congrats! You have found matching elements - {allElements[currElements[0]]}!");
+                        allElements.RemoveAt(currElements[1]);
+                        allElements.RemoveAt(currElements[0]);
+                        
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again!");
+
+                    }
+                }
+                
+
+                if(allElements.Count == 0)
+                {
+                    Console.WriteLine($"You have won in {movesCounter} turns!");
+                    gameIsWon = true;
+                    break;
+
+                    
+                }  
+                input = Console.ReadLine();
+                outOfRange = false;
+            }
+            if(gameIsWon == false)
+            {
+                Console.WriteLine("Sorry you lose :(");
+                Console.WriteLine(string.Join(' ', allElements));
+            }
+            
+            
+        }
+    }
+}
---
+++ ExamPreparation/ShootForTheWin/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+namespace ShootForTheWin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] target = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            string input = Console.ReadLine();
+            int shotTargetsCount = 0;
+            while (input != "End")
+            {
+                int index = int.Parse(input);
+                for (int i = 0; i <= target.Length - 1; i++)
+                {
+                    if (target[i] != -1)
+                    {
+                        if (index > target.Length - 1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            if (target[index] > target[i])
+                            {
+                                target[i] = target[i] + target[index];
+                            }
+                            else if (target[index] < target[i])
+                            {
+                                target[i] = target[i] - target[index];
+
+                            }
+                            //else if (target[index] == target[i])
+                            //{
+                            //    target[index] = target[i] + target[i];
+                            //}
+                        }
+
+                    }
+
+                }
+                if (index <= target.Length - 1)
+                {
+                    target[index] = -1;
+                    shotTargetsCount++;
+                }
+                
+                
+                input = Console.ReadLine();
+            }
+            Console.Write($"Shot targets: {shotTargetsCount} -> {string.Join(' ', target)}");
+            
+            
+        }
+    }
+}
