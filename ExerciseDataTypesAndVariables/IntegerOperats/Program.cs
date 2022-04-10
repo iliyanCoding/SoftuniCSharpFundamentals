50e1ede39e198845911b9d85a57832e9c53e8287
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-04-10 11:37:44 +0300

    ExerciseDataTypesAndVariables

---
+++ ExerciseDataTypesAndVariables/IntegerOperats/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace IntegerOperats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long firstN = long.Parse(Console.ReadLine());
+            long secondN = long.Parse(Console.ReadLine());
+            long thirdN = long.Parse(Console.ReadLine());
+            long fourthN = long.Parse(Console.ReadLine());
+            long sum = firstN + secondN;
+            long devide = sum / thirdN;
+            long multiply = devide * fourthN;
+            Console.WriteLine(multiply);
+        }
+    }
+}
