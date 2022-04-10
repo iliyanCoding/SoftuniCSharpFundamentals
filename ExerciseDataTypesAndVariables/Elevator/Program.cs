4eec839e1faa954c1f1477bcb926a4c6a5abaeff
Iliyan Muradilov <iliyan.programirane@gmail.com>
2022-04-10 11:36:22 +0300

    ExerciseDataTypesAndVariables

---
+++ ExerciseDataTypesAndVariables/Elevator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPeople = int.Parse(Console.ReadLine());
+            int capacityOfPeople = int.Parse(Console.ReadLine());
+
+            int fullcourses = numOfPeople / capacityOfPeople;
+            if(numOfPeople % capacityOfPeople != 0)
+            {
+                fullcourses++;
+            }
+            Console.WriteLine(fullcourses);
+
+        }
+    }
+}
