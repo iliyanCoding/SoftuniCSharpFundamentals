523d2eb25f050c47e811c54992757be39dcb8c57
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-12 20:39:44 +0300

    ExerciseDataTypesAndVariables

---
+++ ExerciseDataTypesAndVariables/SpiceMustFlow/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalExtractedSpices = 0;
+            const int consumedSpices = 26;
+            int daysMined = 0;
+            int startingYield = int.Parse(Console.ReadLine());
+            for (int i = startingYield; i >= 100 ; i -= 10)
+            {
+                int extractedSpices = i;
+                totalExtractedSpices += extractedSpices;
+                if(totalExtractedSpices >= consumedSpices)
+                {
+                    totalExtractedSpices -= consumedSpices;
+                }
+                
+                daysMined++;
+                
+            }
+
+            if (totalExtractedSpices >= consumedSpices)
+            {
+                totalExtractedSpices -= consumedSpices;
+            }
+            Console.WriteLine(daysMined);
+            Console.WriteLine(totalExtractedSpices);
+            
+
+        }
+    }
+}
