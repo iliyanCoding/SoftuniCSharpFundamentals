78ab3184c38e91f70654eb97219856ade3ea17f6
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-12 20:39:37 +0300

    ExerciseDataTypesAndVariables

---
+++ ExerciseDataTypesAndVariables/PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrintPartOfASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndex = int.Parse(Console.ReadLine());
+            int endIndex = int.Parse(Console.ReadLine());
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                char currChar = (char)i;
+                Console.Write(currChar + " ");
+            }
+        }
+    }
+}
