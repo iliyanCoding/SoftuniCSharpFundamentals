3e1540e8ff6fd0762c34eab6747d26c114da4afd
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-12 20:39:54 +0300

    ExerciseDataTypesAndVariables

---
+++ ExerciseDataTypesAndVariables/SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            do
+            {
+                sum += (number % 10);
+                number /= 10;
+            } while (number > 0);
+            Console.WriteLine(sum);
+        }
+    }
+}
