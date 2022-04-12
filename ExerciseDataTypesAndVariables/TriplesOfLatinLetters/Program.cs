27cf97dd8443fc150340c43a321663dd03093a81
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-12 20:40:42 +0300

    ExerciseDataTypesAndVariables

---
+++ ExerciseDataTypesAndVariables/TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TriplesOfLatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (char a = 'a'; a < 'a' + n; a++)
+            {
+                for (char b = 'a'; b < 'a' + n; b++)
+                {
+                    for (char c = 'a'; c < 'a' + n; c++)
+                    {
+                        Console.WriteLine($"{a}{b}{c}");
+                    }
+                }
+            }
+        }
+    }
+}
