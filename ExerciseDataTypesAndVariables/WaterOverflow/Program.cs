1eb226f950e2299bd27b5bbd9a91b0879cc2cc9b
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-13 20:35:02 +0300

    ExerciseDataTypesAndVariables

---
+++ ExerciseDataTypesAndVariables/WaterOverflow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int buckets = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < buckets; i++)
+            {
+                int poured = int.Parse(Console.ReadLine());
+                sum += poured;
+                if(sum > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    sum -= poured;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
