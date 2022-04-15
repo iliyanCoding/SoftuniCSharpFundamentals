1c72d384fdb1d2595b8ad871179a7c5770d2a274
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-15 22:47:59 +0300

    Methods

---
+++ Methods/CalculateRectangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = AreaOfRectangle(width, height);
+            Console.WriteLine(area);
+        }
+
+       static double AreaOfRectangle(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
