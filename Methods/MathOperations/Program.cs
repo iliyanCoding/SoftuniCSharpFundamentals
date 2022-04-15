02c98871d3cc9b8993268c3a8de6afc1ecbd045c
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-15 22:48:30 +0300

    Create Program.cs

---
+++ Methods/MathOperations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            char opperator = char.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            double result = Calculate(firstNum, opperator, secondNum);
+            Console.WriteLine(result);
+        }
+        static double Calculate(int firstNum, char opperator, int secondNum)
+        {
+            double result = 0;
+
+            switch (opperator)
+            {
+                case '+':
+                    result = firstNum + secondNum;
+                    break;
+                case '-':
+                    result = firstNum - secondNum;
+                    break;
+                case '/':
+                    result = firstNum / secondNum;
+                    break;
+                case '*':
+                    result = firstNum * secondNum;
+                    break;
+            }
+            return result;
+        }
+    }
+}
