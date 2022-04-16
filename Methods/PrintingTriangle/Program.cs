1c69e8ff251095a2785457e54cbf800a573320cc
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-16 20:25:17 +0300

    Create Program.cs

---
+++ Methods/PrintingTriangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                PrintLine(1, i);
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i +" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
