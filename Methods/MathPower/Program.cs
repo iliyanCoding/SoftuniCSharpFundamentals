33834211c4447262039eddfe9b864e868374051a
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-16 20:24:17 +0300

    Create Program.cs

---
+++ Methods/MathPower/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double theBase = double.Parse(Console.ReadLine());
+            int thePower = int.Parse(Console.ReadLine());
+            double result = RaiseToPower(theBase, thePower);
+            Console.WriteLine(result);
+        }
+        static double RaiseToPower(double baza, int power)
+        {
+            double result = 0d;
+            result = Math.Pow(baza, power);
+            return result;
+        }
+
+    }
+}
