5f8dcb4183a2cb58a86e55a6c624e47034d1df7d
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-16 20:25:20 +0300

    Create Program.cs

---
+++ Methods/RepeatString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string result = StringRepeat(input, n);
+            Console.WriteLine(result);
+        }
+
+        private static string StringRepeat(string input, int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(input);
+            }
+            return sb.ToString();
+        }
+    }
+}
