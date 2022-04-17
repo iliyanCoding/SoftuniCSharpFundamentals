935f3d39b677ffd5c25582309e4b608a784496ab
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-17 18:28:20 +0300

    Create Program.cs

---
+++ MethodsExercise/SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int result = MinNumber(a, b, c);
+            Console.WriteLine(result);
+            
+        }
+        static int MinNumber(int a, int b, int c)
+        {
+            int[] array = { a, b, c };
+            int small = int.MaxValue;
+            foreach(int x in array)
+            {
+                if(x < small)
+                {
+                    small = x;
+                }
+            }
+            return small;
+        }
+    }
+}
