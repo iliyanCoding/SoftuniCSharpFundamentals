ac6ac6c2d530c031003daed071c3e698ab0414c2
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-17 18:27:58 +0300

    Create Program.cs

---
+++ Methods/SignOFIntegerNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SignOFIntegerNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            signOfnumber(num);
+        }
+
+        static void signOfnumber(int num)
+        {
+            if(num > 0)
+            {
+                Console.WriteLine($"The number {num} is positive.");
+            }
+            else if(num < 0)
+            {
+                Console.WriteLine($"The number {num} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {num} is zero.");
+            }
+        }
+    }
+}
