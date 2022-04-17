0dfaf394d04a1f4ff7d67155b2cbe7b97394afa6
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-17 18:28:07 +0300

    Create Program.cs

---
+++ MethodsExercise/CharactersInRange/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CharactersInRange
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            char inputOne = char.Parse(Console.ReadLine());
+            char inputTwo = char.Parse(Console.ReadLine());
+
+            PrintTheCharsBetweenTwoChars(inputOne, inputTwo);
+        }
+
+        private static void PrintTheCharsBetweenTwoChars(char firstCharacter, char secondCharacter)
+        {
+            int startCharacter = Math.Min(firstCharacter, secondCharacter);
+            int endCharacter = Math.Max(firstCharacter, secondCharacter);
+
+            for (int i = startCharacter + 1; i < endCharacter; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
