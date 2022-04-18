39aa3812f419b3dee6c00397049f28e181712be2
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-18 23:25:00 +0300

    Create Program.cs

---
+++ Objects/02.BigFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02.BigFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger facotorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                facotorial *= i;
+            }
+
+            Console.WriteLine(facotorial);
+        }
+    }
+}
