cb47e497326c494771bd50cc6741b64e8a15bcb4
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-18 23:24:47 +0300

    Create Program.cs

---
+++ MethodsExercise/VowelsCount/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            int result = VowelsCount(input);
+            Console.WriteLine(result);
+        }
+        static int VowelsCount(string input)
+        {
+            char[] chars = input.ToCharArray();
+
+            int count = 0;
+            foreach (char x in chars)
+            {
+                switch (x)
+                {
+                    case 'a':
+                        count++;
+                        break;
+                    case 'e':
+                        count++;
+                        break;
+                    case 'o':
+                        count++;
+                        break;
+                    case 'u':
+                        count++;
+                        break;
+                    case 'i':
+                        count++;
+                        break;
+                }
+            }
+            return count;
+        }
+    }
+}
