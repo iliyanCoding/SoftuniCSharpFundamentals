6aa52c5416abcd4db33ac7c2541562e4f3e3700c
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-18 23:24:55 +0300

    Create Program.cs

---
+++ Objects/01.Objects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int currWordIndex = random.Next(0, words.Length);
+                string currWord = words[i];
+                words[i] = words[currWordIndex];
+                words[currWordIndex] = currWord;
+            }
+
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
