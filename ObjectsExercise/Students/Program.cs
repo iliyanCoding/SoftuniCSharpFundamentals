a31e6470fb17b307c465f6208f70ef5c62f8f101
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-20 21:55:03 +0300

    Create Program.cs

---
+++ ObjectsExercise/Students/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] studentArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string currStudentFirstName = studentArgs[0];
+                string currStudentLastName = studentArgs[1];
+                double currStudentGrade = double.Parse(studentArgs[2]);
+
+                Student currStudent = new Student(currStudentFirstName, currStudentLastName, currStudentGrade);
+                students.Add(currStudent);
+            }
+
+            //Can be ommitted
+            List<Student> orderedStudents = students.OrderByDescending(s => s.Grade).ToList();
+
+            foreach (Student student in orderedStudents)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            //Always called when new student is beeing created
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}: {this.Grade:f2}";
+        }
+    }
+}
