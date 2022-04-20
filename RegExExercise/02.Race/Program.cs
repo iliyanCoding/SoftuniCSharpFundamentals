ee0881d65d5ec58d9f88a94a4a3b80c83db388fd
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-20 21:55:27 +0300

    Create Program.cs

---
+++ RegExExercise/02.Race/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] participants = Console.ReadLine().Split(", ");
+            List<string> paticipantsWhoHaveChanceToWin = new List<string>();
+            string input = Console.ReadLine();
+            while (input != "end of the race")
+            {
+                string participant = input;
+                string nameRegex = @"[A-z]";
+
+                MatchCollection nameMatch = Regex.Matches(participant, nameRegex);
+                string name = string.Empty;
+                foreach (Match match in nameMatch)
+                {
+                    name += match.Value;
+                }
+
+                if (participant.Contains(name))
+                {
+                    paticipantsWhoHaveChanceToWin.Add(name);
+                }
+
+                string kilometersRegex = @"[0-9]";
+                MatchCollection kilometersMatch = Regex.Matches(participant, kilometersRegex);
+                string stringKilometers = string.Empty;
+                foreach (Match distance in kilometersMatch)
+                {
+                    stringKilometers += distance;
+                }
+                int kilometers = int.Parse(stringKilometers);
+            }
+        }
+    }
+}
