b79545c5f2a88cd759f472217891f6b295cb37d4
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-26 20:27:51 +0300

    Create Program.cs

---
+++ RegularExpressions/01.MatchFullName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b";
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Value + " ");
+            }
+        }
+    }
+}
