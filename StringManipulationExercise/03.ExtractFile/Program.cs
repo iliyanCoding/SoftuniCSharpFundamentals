14afb5ecbdfc8be4e9a6151ecb0611a21e1feb02
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-28 17:41:55 +0300

    Create Program.cs

---
+++ StringManipulationExercise/03.ExtractFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToAFile = Console.ReadLine();
+
+            int startingIndexOtTheFileName = pathToAFile.LastIndexOf('\\');
+            int endingIndexOtTheFileName = pathToAFile.IndexOf('.');
+
+            string fileName = pathToAFile.Substring(startingIndexOtTheFileName + 1, endingIndexOtTheFileName -
+                startingIndexOtTheFileName - 1);
+            string extension = pathToAFile.Substring(endingIndexOtTheFileName + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
