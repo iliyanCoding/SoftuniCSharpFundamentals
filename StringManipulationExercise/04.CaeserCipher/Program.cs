9efb99ead922646624bce7d2b3a2cde8ac2ab1dd
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-28 17:42:00 +0300

    Create Program.cs

---
+++ StringManipulationExercise/04.CaeserCipher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04.CaeserCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string cipher = string.Empty;
+            foreach (var symbol in text)
+            {
+                char decryptedSymbol = (char)(symbol + 3);
+                cipher += decryptedSymbol;
+            }
+            Console.WriteLine(cipher);
+        }
+    }
+}
