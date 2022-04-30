f13cd90186048c21471a2c27e19c640a03522504
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-30 17:13:44 +0300

    Create Program.cs

---
+++ StringManipulationExercise/07.StringExplosion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string field = Console.ReadLine();
+            int bomb = 0;
+            for (int i = 0; i < field.Length; i++)
+            {
+                if (bomb > 0 && field[i] != '>')
+                {
+                    field = field.Remove(i, 1); // Remove char on this index
+                    bomb--; // One bomb is used
+                    i--; // after remove next char is moved 1 position, so return counter i to this char, decreasing i by 1  
+                }
+                else if (field[i] == '>')
+                {
+                    bomb += int.Parse(field[i + 1].ToString()); // takes the digit after '>' - bomb strength and add to bomb
+                }
+            }
+            Console.WriteLine(field);
+        }
+    }
+    
+}
