e29ab08b7e196b2f2c09bfa4f99d1bad282241e6
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-30 17:13:38 +0300

    Create Program.cs

---
+++ StringManipulationExercise/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine().ToList();
+
+            for (int i = 0; i < str.Count - 1; i++)
+            {
+                char curr = str[i];
+                char next = str[i + 1];
+                if (curr == next)
+                {
+                    str.RemoveAt(i);
+                    //str.Remove(str[i]);  така не работи правилно ,защо ?
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join("", str));
+
+        }
+    }
+}
