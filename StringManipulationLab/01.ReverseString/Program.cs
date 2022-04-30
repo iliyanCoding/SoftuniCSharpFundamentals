08a7edab37cc5e82102757063838f26dd5526a80
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-30 17:13:56 +0300

    Create Program.cs

---
+++ StringManipulationLab/01.ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string line = Console.ReadLine();
+            while(line != "end")
+            {
+                string reversed = string.Empty;
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    reversed += line[i];
+                }
+                Console.WriteLine($"{line} = {reversed}");
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
