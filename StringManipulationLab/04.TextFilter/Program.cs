6fbcac8fa50f6fa31cd2c738888286cebe227711
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-02 13:48:59 +0300

    Create Program.cs

---
+++ StringManipulationLab/04.TextFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
