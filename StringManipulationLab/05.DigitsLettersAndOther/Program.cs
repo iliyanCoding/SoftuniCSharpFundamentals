c7dfec95675db12093f878842ff1d7714e93585c
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-02 13:49:03 +0300

    Create Program.cs

---
+++ StringManipulationLab/05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace _05.DigitsLettersAndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder letters = new StringBuilder();
+            StringBuilder numbers = new StringBuilder();
+            StringBuilder otherCharacters = new StringBuilder();
+
+            string text = Console.ReadLine();
+
+            foreach (char item in text)
+            {
+                if(char.IsLetter(item))
+                {
+                    letters.Append(item);
+                }
+                else if(char.IsDigit(item))
+                {0
+                    numbers.Append(item);
+                }
+                else
+                {
+                    otherCharacters.Append(item);
+                }
+            }
+
+            Console.WriteLine(numbers.ToString());
+            Console.WriteLine(letters.ToString());
+            Console.WriteLine(otherCharacters.ToString());
+            
+            
+        }
+    }
+}
