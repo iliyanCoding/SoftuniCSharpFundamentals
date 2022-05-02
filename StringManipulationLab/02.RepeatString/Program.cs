91c35f945117f5cfc637b290581b3e0f4cab3357
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-02 13:48:47 +0300

    Create Program.cs

---
+++ StringManipulationLab/02.RepeatString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i]);
+                }
+            }
+        }
+    }
+}
